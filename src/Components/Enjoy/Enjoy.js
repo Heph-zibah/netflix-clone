222c9ad0e457697e79c6cf6626611b82bcef7afc
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-20 15:12:08 +0100

    created enjoy folder

---
+++ src/Components/Enjoy/Enjoy.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Enjoy = () => {
+	return (
+		<div>
+			<h1>Enjoy on your TV.</h1>
+			<p>
+				Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
+				players, and more.
+			</p>
+		</div>
+	);
+};
+
+export default Enjoy;
