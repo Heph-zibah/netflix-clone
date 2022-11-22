ed1166c7f9eb40ecd39e3375051fd896d8b29067
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-11-22 08:31:38 +0100

    added watch folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Enjoy from './Components/Enjoy/Enjoy';
 import Hero from './Components/Hero/Hero';
+import Watch from './Components/Watch/Watch';
 
 function App() {
 	return (
 		<div className='App'>
 			<Hero />
 			<Enjoy />
+			<Watch />
 		</div>
 	);
 }
